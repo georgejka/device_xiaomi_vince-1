1a66d85fcf6ec55d9f6e4af0688af30ff95ba40a
adnan-44 <mohd.adnan0405@gmail.com>
2021-10-17 22:21:48 +0530

    vince: device: drop Bootjars
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

--- device.mk
+++ device.mk
@@ -383,8 +383,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
@@ -478,5 +478,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
