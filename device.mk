root2raj/device_xiaomi_vince

vince: Add basic USB HAL that reports no status change

device.mk
@@ -384,7 +384,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.vince
 
 # Vibrator
 PRODUCT_PACKAGES += \

georgejka/device_xiaomi_vince-1

Signed-off-by: ItsVixano <vixanonew@gmail.com>
Change-Id: I46874b1d6a6491d07b7d5ae896a192312b4047cd

device.mk
@@ -448,7 +448,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.msm8953
 
 # Vibrator
 PRODUCT_PACKAGES += \